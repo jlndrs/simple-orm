568572aa27228c35d7036bab840a3bcf499926fe
Julian Drees <julian-drees@gmx.de>
2020-05-09 16:48:12 +0200

    AbstractPersistenceService removed unused methods

--- src/main/java/de/juliandrees/simpleorm/persistence/AbstractPersistenceService.java
+++ src/main/java/de/juliandrees/simpleorm/persistence/AbstractPersistenceService.java
@@ -1,18 +1,11 @@
 package de.juliandrees.simpleorm.persistence;
 
 import de.juliandrees.simpleorm.entity.EntityManager;
-import de.juliandrees.simpleorm.entity.EntityScheme;
-import de.juliandrees.simpleorm.entity.PropertyMapping;
 import de.juliandrees.simpleorm.persistence.sql.SqlConnection;
 import lombok.AccessLevel;
 import lombok.Getter;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * // TODO class description
@@ -38,39 +31,6 @@ public abstract class AbstractPersistenceService implements PersistenceService {
     @Override
     public void onInitialize() {
 
-    }
-
-    protected <T> T buildEntity(ResultSet resultSet, Class<?> entityClass, EntityScheme scheme) throws Exception {
-        T instance = null;
-        if (resultSet.next()) {
-            instance = (T) entityClass.getConstructor().newInstance();
-            for (Map.Entry<String, PropertyMapping> entry : scheme.getPropertyMappings().entrySet()) {
-                Object value = resultSet.getObject(entry.getKey(), entry.getValue().getFieldType());
-                entry.getValue().getSetter().invoke(instance, value);
-            }
-        }
-        return instance;
-    }
-
-    protected <T> List<T> buildEntities(ResultSet resultSet, Class<?> entityClass, EntityScheme scheme) throws Exception {
-        int rowCount = 0;
-        if (resultSet != null) {
-            resultSet.last();
-            rowCount = resultSet.getRow();
-            resultSet.beforeFirst();
-        }
-        List<T> entities = new ArrayList<>();
-        for (int i = 0; i < rowCount; i++) {
-            entities.add(buildEntity(resultSet, entityClass, scheme));
-        }
-        return entities;
-    }
-    protected EntityScheme getEntityScheme(Class<?> entityClass) {
-        Optional<EntityScheme> optionalScheme = getEntityManager().getEntityScheme(entityClass);
-        if (optionalScheme.isEmpty()) {
-            throw new IllegalArgumentException("entity class not mapped: " + entityClass.getName());
-        }
-        return optionalScheme.get();
     }
 
     @Override
